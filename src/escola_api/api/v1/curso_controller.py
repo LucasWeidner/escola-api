3aedacbc8d86b45500ceb2db9a97f19df5d815a3
Lucas Weidner <lucasmweidner@gmail.com>
2025-06-04 21:19:05 -0300

    Atualização aula

--- src/escola_api/api/v1/curso_controller.py
+++ src/escola_api/api/v1/curso_controller.py
@@ -1,6 +1,11 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+
+from src.escola_api.database.modelos import CursoEntidade
 from src.escola_api.schemas.curso_schemas import Curso, CursoCadastro, CursoEditar
 from src.escola_api.app import router
+from src.escola_api.database.banco_dados import SessionLocal # Função de depedência para obter uma sessão do banco de dados
 
 cursos = [
     # instanciando um objeto da classe Curso
@@ -9,9 +14,18 @@ cursos = [
 ]
 
 
+def get_db():
+    db = SessionLocal() #Cria uma nova sessão do banco de dados
+    try:
+        yield db # Retorna a sessão de froma que o FastAPI possa utiliza-la nos rotas
+    finally:
+        db.close() # Garante que a sessão será fechada após o uso
+
+
 # localhost:8000/docs
 @router.get("/api/cursos")
-def listar_todos_cursos():
+def listar_todos_cursos(db: Session = Depends(get_db)):
+    cursos = db.query(CursoEntidade).all()
     return cursos
 
 
